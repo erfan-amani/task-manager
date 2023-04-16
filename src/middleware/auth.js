f15b901933a1133d3c38782d3cf078e486f51a7d
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-04-16 20:03:07 +0330

    added user auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../model/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const bearer = req.header("Authorization");
+    const token = bearer.split(" ")[1];
+
+    const decoded = jwt.verify(token, "taskmanagerapp");
+    const user = await User.findOne({ _id: decoded.id });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Not authorized!");
+  }
+};
+
+module.exports = userAuth;
